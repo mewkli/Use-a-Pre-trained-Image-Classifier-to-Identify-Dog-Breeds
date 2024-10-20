e1a3ce942353bb34d6e3618f5f9702e824e3095b
mewkli <132446268+mewkli@users.noreply.github.com>
2024-10-20 08:58:30 +0530

    Update classify_images.py

--- Udacity/classify_images.py
+++ Udacity/classify_images.py
@@ -74,6 +74,6 @@ def classify_images(images_dir, results_dic, model):
        
         truth = results_dic[key][0]   
         if truth in model_label:
-            results_dic[key].extend((model_label, 1))
+            results_dic[key].extend([model_label, 1])
         else:
-            results_dic[key].extend((model_label, 0))
+            results_dic[key].extend([model_label, 0])
